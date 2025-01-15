234c59369c740d801930e9c63c62ccdbb3f6f3e0
Okinea Dev <hi@okinea.dev>
2025-01-15 14:29:21 +0200

    feat(devserver): ✨ allow to set sample contributors count
    * Updated the `devServer` to accept a query parameter for the number of contributors.
    * The default value is set to 100 if no parameter is provided.
    * This change enhances flexibility in testing the contributors table generation.

--- dev/server.ts
+++ dev/server.ts
@@ -1,12 +1,14 @@
 import sampleContributor from '../samples/sampleContributor'
 import { generateContributorsTable } from '../src'
 
-const sampleContributors = Array(100).fill(sampleContributor)
-
 const devServer = Bun.serve({
 	async fetch(req: Request): Promise<Response> {
 		const url = new URL(req.url)
 		const params = Object.fromEntries(url.searchParams.entries())
+
+		const sampleContributors = Array(Number(params.contributors) || 100).fill(
+			sampleContributor,
+		)
 
 		const image = await generateContributorsTable(sampleContributors, {
 			ssr: false,
